a925f589536a752d0a6237cb01df1ce5a4fb1162
mefi92 <sgrcsont@gmail.com>
2024-08-26 21:30:49 +0200

    AddProduct_ShouldAddProductSuccessfully mock test.

--- Kamra.Core/Services/ProductService.cs
+++ Kamra.Core/Services/ProductService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kamra.Core.Interfaces;
+using Kamra.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,28 @@ using System.Threading.Tasks;
 
 namespace Kamra.Core.Services
 {
-    internal class ProductService
+    public class ProductService : IProductService
     {
+        private List<Product> products = new List<Product>();
+
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }        
+
+        public void RemoveProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
