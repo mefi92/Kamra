2ea14bb60efe0c5a07504a83bfea76ff2a8c79e0
mefi92 <sgrcsont@gmail.com>
2024-09-01 11:55:52 +0200

    IPersistence interface created.

---
+++ Kamra.Core/Interfaces/IPersistence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamra.Core.Interfaces
+{
+    public interface IPersistence<T>
+    {
+        void Add(T entity);
+        T GetByName(string name);
+        IEnumerable<T> GetAll();
+        void Remove(T entity);
+        void Update(T entity);
+    }
+}
