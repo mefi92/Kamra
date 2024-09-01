8c22a3c5d2ba6f240317b57183234be75595027b
mefi92 <sgrcsont@gmail.com>
2024-09-01 12:24:57 +0200

    InMemoryPersitence created with defined methods.

---
+++ Karma.Persistence/InMemoryPersistence.cs
@@ -0,0 +1,40 @@
+﻿using Kamra.Core.Interfaces;
+using System.Net.Http.Headers;
+
+namespace Karma.Persistence
+{
+    public class InMemoryPersistence<T> : IPersistence<T>
+    {
+        private readonly List<T> _entities = new List<T>();
+
+        public void Add(T entity)
+        {
+            _entities.Add(entity);
+        }
+
+        public T GetByName(string name)
+        {
+            return _entities.FirstOrDefault(e => e.GetType().GetProperty("Name").GetValue(e, null).ToString() == name);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _entities.AsReadOnly();
+        }        
+
+        public void Remove(T entity)
+        {
+            _entities.Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            var existingEntity = GetByName(entity.GetType().GetProperty("Name").GetValue(entity, null).ToString());
+            if (existingEntity != null)
+            {
+                _entities.Remove(existingEntity);
+                _entities.Add(entity);
+            }
+        }
+    }
+}
